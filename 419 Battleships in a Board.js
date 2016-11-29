4cf1063c02cfc07efa82eef31142f3380ba3a425
StockDC2 <StockDC2@gmail.com>
2016-11-28 19:42:26 -0800

    #419

---
+++ 419 Battleships in a Board.js
@@ -0,0 +1,166 @@
+/**
+ * @param {character[][]} board
+ * @return {number}
+ */
+
+// var createGrid = function(board) {
+// 	var main = [],
+// 		inner;
+
+// 	for (var i = 0; i < board.length; i++) {
+// 		inner = [];
+// 		for (var j = 0; j < board[i].length; j++) {
+// 			inner.push(board[i][j]);
+// 		}
+// 		main.push(inner);
+// 	}
+
+// 	return main;
+// }
+
+// var findShip = function(i, j, board) {
+// 	if (i + 1 < board.length && board[i + 1][j] === 'X') {
+// 		findShip(i + 1, j, board);
+// 	}
+
+// 	if (j + 1 < board[i].length && board[i][j + 1] === 'X') {
+// 		findShip(i, j + 1, board);
+// 	}
+// }
+
+// var countBattleships = function(board) {
+// 	var count = 0;
+
+// 	board = createGrid(board);
+
+//     for (var i = 0; i < board.length; i++) {
+//     	for (var j = 0; j < board[i].length; j++) {
+//     		if (board[i][j] === 'X') {
+
+//     		}
+//     	}
+//     }
+
+//     return count;
+// };
+
+
+// // Does not modify the board but creates an object to keep track of visited values.
+// var createGrid = function(board) {
+// 	var main = [],
+// 		inner;
+
+// 	for (var i = 0; i < board.length; i++) {
+// 		inner = [];
+// 		for (var j = 0; j < board[i].length; j++) {
+// 			inner.push(board[i][j]);
+// 		}
+// 		main.push(inner);
+// 	}
+
+// 	return main;
+// }
+
+// var findShip = function(i, j, board, visited) {
+// 	if (visited[i.toString() + j.toString()]) {
+// 		return;
+// 	}
+
+// 	visited[i.toString() + j.toString()] = true;
+
+// 	if (i + 1 < board.length && board[i + 1][j] === 'X') {
+// 		findShip(i + 1, j, board, visited);
+// 	}
+
+// 	if (i - 1 >= 0 && board[i - 1][j] === 'X') {
+// 		findShip(i - 1, j, board, visited);
+// 	}
+
+// 	if (j + 1 < board[i].length && board[i][j + 1] === 'X') {
+// 		findShip(i, j + 1, board, visited);
+// 	}
+
+// 	if (j - 1 >= 0 && board[i][j - 1] === 'X') {
+// 		findShip(i, j - 1, board, visited);
+// 	}
+// }
+
+// var countBattleships = function(board) {
+// 	var count = 0,
+// 		visited = {};
+
+// 	board = createGrid(board);
+
+//     for (var i = 0; i < board.length; i++) {
+//     	for (var j = 0; j < board[i].length; j++) {
+//     		if (board[i][j] === 'X' && !visited[i.toString() + j.toString()]) {
+//     			findShip(i, j, board, visited);
+//     			count++;
+//     		}
+//     	}
+//     }
+
+//     return count;
+// };
+
+
+// // Modifies the board but counts ships in one pass.
+// var createGrid = function(board) {
+// 	var main = [],
+// 		inner;
+
+// 	for (var i = 0; i < board.length; i++) {
+// 		inner = [];
+// 		for (var j = 0; j < board[i].length; j++) {
+// 			inner.push(board[i][j]);
+// 		}
+// 		main.push(inner);
+// 	}
+
+// 	return main;
+// }
+
+// var findShip = function(i, j, board) {
+// 	board[i][j] = '.';
+
+// 	if (i + 1 < board.length && board[i + 1][j] === 'X') {
+// 		findShip(i + 1, j, board);
+// 	}
+
+// 	if (i - 1 >= 0 && board[i - 1][j] === 'X') {
+// 		findShip(i - 1, j, board);
+// 	}
+
+// 	if (j + 1 < board[i].length && board[i][j + 1] === 'X') {
+// 		findShip(i, j + 1, board);
+// 	}
+
+// 	if (j - 1 >= 0 && board[i][j - 1] === 'X') {
+// 		findShip(i, j - 1, board);
+// 	}
+// }
+
+// var countBattleships = function(board) {
+// 	var count = 0;
+
+// 	board = createGrid(board);
+
+//     for (var i = 0; i < board.length; i++) {
+//     	for (var j = 0; j < board[i].length; j++) {
+//     		if (board[i][j] === 'X') {
+//     			findShip(i, j, board);
+//     			count++;
+//     		}
+//     	}
+//     }
+
+//     return count;
+// };
+
+
+var myBoard = [
+'X..X',
+'...X',
+'...X']
+
+console.log(countBattleships(myBoard));
